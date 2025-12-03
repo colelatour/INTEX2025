b9063060fddc0cba463b8b4cd747f0ac2070a608
John Everett <john.everett32@gmail.com>
2025-12-03 12:27:10 -0700

    added ssl to db connection

--- db.js
+++ db.js
@@ -5,7 +5,8 @@ const knex = require('knex')({
     user: process.env.RDS_USERNAME || 'postgres',
     password: process.env.RDS_PASSWORD || 'clatour0',
     database: process.env.RDS_DB_NAME || 'ellarises',
-    port: process.env.RDS_PORT || 5432
+    port: process.env.RDS_PORT || 5432,
+    ssl: { rejectUnauthorized: false }
   },
   pool: {
     min: 2,
