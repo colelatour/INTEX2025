8a2dfccd554f0628f2a161551803f4fe562dfed1
John Everett <john.everett32@gmail.com>
2025-12-03 12:08:51 -0700

    changes to db connection

--- db.js
+++ db.js
@@ -1,5 +1,16 @@
-const knexfile = require('./knexfile');
-const environment = process.env.NODE_ENV || 'development';
-const knex = require('knex')(knexfile[environment]);
+const knex = require('knex')({
+  client: 'postgresql',
+  connection: {
+    host: process.env.RDS_HOSTNAME || 'localhost',
+    user: process.env.RDS_USERNAME || 'postgres',
+    password: process.env.RDS_PASSWORD || 'clatour0',
+    database: process.env.RDS_DB_NAME || 'ellarises',
+    port: process.env.RDS_PORT || 5432
+  },
+  pool: {
+    min: 2,
+    max: 10
+  }
+});
 
 module.exports = knex;
